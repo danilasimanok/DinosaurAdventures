222004793302cdb2bb0eca8d95c442e101c69611
danilasimanok <dsimanok@bk.ru>
2016-12-28 19:36:12 +0300

    :(

--- app/src/main/java/com/example/student2/dinosauradventures/Dino.java
+++ app/src/main/java/com/example/student2/dinosauradventures/Dino.java
@@ -23,21 +23,15 @@ public class Dino extends View {
     public Dino(Context context){
         super(context);
         //Присвоили битмапам соответствующие картинки
-        bird=BitmapFactory.decodeResource(getResources(),R.drawable.birddino);
-        yee=BitmapFactory.decodeResource(getResources(),R.drawable.yee);
-        pig=BitmapFactory.decodeResource(getResources(),R.drawable.pig);
-        google=BitmapFactory.decodeResource(getResources(),R.drawable.googledino);
+
         //Делаем оболочку для ресурсов
-        bSrc=new Rect(0,0,bird.getWidth(),bird.getHeight());
-        ySrc=new Rect(0,0,yee.getWidth(),yee.getHeight());
-        pSrc=new Rect(0,0,pig.getWidth(),pig.getHeight());
-        gSrc=new Rect(0,0,google.getWidth(),google.getHeight());
+
     }
 
     //Рисуем
     @Override
     protected void onDraw(Canvas canvas){
-        t++;
+
     }
 
     //Обрабатываем касания
---
+++ app/src/main/java/com/example/student2/dinosauradventures/KaptuHka.java
@@ -0,0 +1,10 @@
+package com.example.student2.dinosauradventures;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by student2 on 28.12.16.
+ */
+public class KaptuHka {
+    private Bitmap kartina;
+}
