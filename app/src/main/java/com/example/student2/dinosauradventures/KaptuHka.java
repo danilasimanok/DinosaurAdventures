608d943528f8de3ef1f353ade5fa836fcb5b0ab2
danilasimanok <dsimanok@bk.ru>
2016-12-29 18:06:55 +0300

    :(

--- app/src/main/java/com/example/student2/dinosauradventures/KaptuHka.java
+++ app/src/main/java/com/example/student2/dinosauradventures/KaptuHka.java
@@ -1,10 +1,24 @@
 package com.example.student2.dinosauradventures;
 
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
 
 /**
  * Created by student2 on 28.12.16.
  */
 public class KaptuHka {
-    private Bitmap kartina;
+    public Bitmap kartina;
+    public Rect src,dst;
+    public Paint paint;
+    public void draw(Rect dst, Paint paint, Canvas canvas){
+        this.dst=dst;
+        this.paint=paint;
+        canvas.drawBitmap(this.kartina,this.src,this.dst,this.paint);
+    }
+    public KaptuHka(Bitmap kartina,Rect src){
+        this.kartina=kartina;
+        this.src=src;
+    }
 }
